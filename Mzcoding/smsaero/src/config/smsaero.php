3a7e4ba5d53616a4f868cd1ef09c8674b25eefd4
Stanislav <mzcoding@gmail.com>
2016-06-30 14:07:14 +0300

    Delete smsaero.php

--- Mzcoding/smsaero/src/config/smsaero.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-return [
-  'login' => '', //Ваш логин на http://smsaero.ru/
-  'password' => '', //Ваш пароль на http://smsaero.ru/
-  'sign' => 'NIKE', //Подпись по умолчанию
-  'url'  => 'gate.smsaero.ru'
-];
