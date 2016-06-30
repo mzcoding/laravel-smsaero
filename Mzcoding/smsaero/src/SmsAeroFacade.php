6f5315bbe29215ee50216ef2a987f02157d68b6e
Stanislav <mzcoding@gmail.com>
2016-06-30 14:06:53 +0300

    Delete SmsAeroFacade.php

--- Mzcoding/smsaero/src/SmsAeroFacade.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Mzcoding\SmsAero;
-
-use Illuminate\Support\Facades\Facade;
-
-class SmsAeroFacade extends Facade{
-     protected static function getFacadeAccessor(){
-        return 'smsaero';
-     }
-}
